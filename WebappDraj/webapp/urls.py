05d3bd064fe2e7f1274754c0a25a64dd583e3319
Danny <danny.dhs@gmail.com>
2015-12-16 15:21:57 -0500

    add modification

--- WebappDraj/webapp/urls.py
+++ WebappDraj/webapp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.inf_estad_list, name='first-view'),
+	# url(r'^$', views.inf_estad_list, name='first-view'),
 
 	url(r'^$', views.search, name='search'),
 ]
